095d872c51d4926931415aade08d906590a717d1
IC3CLD <67324553+IC3CLD@users.noreply.github.com>
2020-09-04 20:45:56 +0300

    Правки

--- js/task-6.js
+++ js/task-6.js
@@ -1,8 +1,33 @@
 let input = confirm(`Введите число`);
 let total = 0;
 
-while (input) {
-input = +prompt();
-total += input;
-}
+// while (input) {
+// input = +prompt();
+// total += input;
+// }
+// alert (`Общая сумма чисел равна ${total} `);
+
+// input = prompt();
+// while (input !== null) {
+//   if (isNaN(input)) {
+//     alert("Было введено не число, попробуйте еще раз");
+//     input = prompt();
+//   } else {
+//     input = +input;
+//     total += input;
+//     input = prompt();
+//   }
+// }
+// alert(`Общая сумма чисел равна ${total} `);
+
+do {
+    input = prompt ();
+     if (isNaN(input)){
+        alert ('Было введено не число, попробуйте еще раз');
+    } else {
+        total += +input;
+    }   
+}while (input !== null);
 alert (`Общая сумма чисел равна ${total} `);
+
+
